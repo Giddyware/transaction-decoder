685a57bc37cbee854fd06f7516a1ee9ac8e51d0d
Giddyware <idowugidiyanuoluwa@gmail.com>
2025-01-08 04:45:28 +0100

    feat: commandline argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use clap::Parser;
 use std::error::Error;
 use std::io::{Error as ioError, Read};
 use transaction::{Amount, Input, Output, Transaction, Txid};
 mod transaction;
 use sha2::{Digest, Sha256};
+
+#[derive(Parser)]
+#[command(name = "Transaction Decoder")]
+#[command(version = "1.0")]
+#[command(about="Bitcoin Transaction Decoder", long_about=None)]
+struct Cli {
+    #[arg(required = true, help = "(string, required) Raw transaction hex")]
+    transaction_hex: String,
+}
 
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> Result<u64, ioError> {
     let mut compact_size = [0_u8; 1];
@@ -115,9 +125,10 @@ fn decode(transaction_hex: String) -> Result<String, Box<dyn Error>> {
 }
 
 fn main() {
-    let transaction_hex = "0100000002f544d5a6ee6d25a1cc1c54c01f7c4e4a8b65191ac64f9a0c3c98b5421245642c000000006b483045022100c3a359416a552c4a115bc886870e1b060f05145d6112154de348b75523eb4d9d022066bc22f5dcee84e626e5d42bfa937058ebd6d01a3cf804be3a4b7a986a15d3fa012103e7c50bb4c10bf167e9840b75d924f09f0034f20334f301f6d652c1c059219650ffffffff61070d2983b3a9221b4eedfd46db990d38e0f039d25bb37877e4fa94cdceca52000000006b483045022100fcb0bfa709cb86541698b793eec7560d5da395e01018c9f50adaed06904440db022058fb78aaaddf20e273ee351833d74597848407c203c82455d2fbfcbded21c840012103e7c50bb4c10bf167e9840b75d924f09f0034f20334f301f6d652c1c059219650ffffffff02e0f70300000000001976a9149f21a07a0c7c3cf65a51f586051395762267cdaf88ac915f4000000000001976a9149f21a07a0c7c3cf65a51f586051395762267cdaf88ac00000000";
+    // let transaction_hex = "0100000002f544d5a6ee6d25a1cc1c54c01f7c4e4a8b65191ac64f9a0c3c98b5421245642c000000006b483045022100c3a359416a552c4a115bc886870e1b060f05145d6112154de348b75523eb4d9d022066bc22f5dcee84e626e5d42bfa937058ebd6d01a3cf804be3a4b7a986a15d3fa012103e7c50bb4c10bf167e9840b75d924f09f0034f20334f301f6d652c1c059219650ffffffff61070d2983b3a9221b4eedfd46db990d38e0f039d25bb37877e4fa94cdceca52000000006b483045022100fcb0bfa709cb86541698b793eec7560d5da395e01018c9f50adaed06904440db022058fb78aaaddf20e273ee351833d74597848407c203c82455d2fbfcbded21c840012103e7c50bb4c10bf167e9840b75d924f09f0034f20334f301f6d652c1c059219650ffffffff02e0f70300000000001976a9149f21a07a0c7c3cf65a51f586051395762267cdaf88ac915f4000000000001976a9149f21a07a0c7c3cf65a51f586051395762267cdaf88ac00000000";
 
-    match decode(transaction_hex.to_string()) {
+    let cli = Cli::parse();
+    match decode(cli.transaction_hex) {
         Ok(json) => println!("{}", json),
         Err(e) => eprintln!("{}", e),
     }
